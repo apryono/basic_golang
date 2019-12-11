4e0e67b7450e1e353b398278b802d41366443fae
apryono <apryonoboang@gmail.com>
2019-12-11 14:00:11 +0700

    updated

--- flowControl/condition/condition.go
+++ flowControl/condition/condition.go
@@ -16,16 +16,34 @@ import "fmt"
 
 */
 func main() {
-	a := 5
+	// a := 5
 
-	fmt.Println("sebelum if")
-	//Materi Mengenai if, else, dan else if
-	if a < 10 && a >= 5 {
-		fmt.Println("Perintah di eksekusi")
-	} else if a == 5 {
-		fmt.Println("Perintah tidak di eksekusi")
+	// fmt.Println("sebelum if")
+	// //Materi Mengenai if, else, dan else if
+	// if a < 10 && a >= 5 {
+	// 	fmt.Println("Perintah di eksekusi")
+	// } else if a == 5 {
+	// 	fmt.Println("Perintah tidak di eksekusi")
+	// } else {
+	// 	fmt.Println("Final Else")
+	// }
+	// fmt.Println("Setelah if")
+
+	/*
+		%.1f = hanya menentukan satu digit di belakang koma contoh 0.2
+		%.2f = hanya menentukan satu digit di belakang koma contoh 0.20
+		%f => untuk nilai yang hasil nya float
+		%s => untuk nilai hasil nya string
+		%v => default ,dapat di gunakan untuk keseluruhan
+	*/
+	var point = 6840.0
+
+	if percent := point / 100; percent >= 100 {
+		fmt.Printf("%.1v%s perfect!\n", percent, "%")
+	} else if percent >= 70 {
+		fmt.Printf("%.1f%s good\n", percent, "%")
 	} else {
-		fmt.Println("Final Else")
+		fmt.Printf("%.1f%s not bad\n", percent, "%")
 	}
-	fmt.Println("Setelah if")
+
 }
